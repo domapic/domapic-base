db17fa94676f57a34b21100facb01ec4c8bb183e
Javier Brea <javier.brea@gmail.com>
2017-11-28 19:35:20 +0100

    Remove chai-as-promised dependency

--- test/test.js
+++ test/test.js
@@ -1,13 +1,11 @@
 /* global describe, it, before, beforeEach, after, afterEach */
 
 const chai = require('chai')
-// const chaiAsPromised = require('chai-as-promised')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const dirtyChai = require('dirty-chai')
 
 chai.use(sinonChai)
-// chai.use(chaiAsPromised)
 chai.use(dirtyChai)
 
 module.exports = {
