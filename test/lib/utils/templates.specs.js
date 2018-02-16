3e5ce0ab02895d5ba3e3ca072ac15cccedd73bbf
Javier Brea <javier.brea@gmail.com>
2018-02-16 09:19:07 +0100

    Add templates coverage

--- test/lib/utils/templates.specs.js
+++ test/lib/utils/templates.specs.js
@@ -6,12 +6,91 @@ const test = require('../../index')
 const templates = require('../../../lib/utils/templates')
 const cliTemplates = require('../../../lib/utils/templates/cli')
 
+const PRE_COMPILED_TEMPLATES = ['cli', 'client', 'info', 'openapi', 'paths', 'process', 'server', 'service', 'storage']
+
 test.describe('Utils -> templates', () => {
   test.describe('compile', () => {
     test.it('should return a template function for each received template', () => {
       const compiledTemplates = templates.compile(cliTemplates)
       _.each(cliTemplates, (stringTemplate, templateName) => {
         test.expect(compiledTemplates[templateName]).to.be.a('function')
+      })
+    })
+  })
+
+  test.describe('compiled', () => {
+    test.it('should return an object containing all core precompiled templates', () => {
+      _.each(PRE_COMPILED_TEMPLATES, (templatesScope) => {
+        test.expect(templates.compiled[templatesScope]).to.be.an('object')
+        if (templatesScope === 'cli') {
+          _.each(templates.compiled[templatesScope], (template, templateName) => {
+            test.expect(template).to.be.a('function')
+          })
+        }
+      })
+    })
+  })
+
+  test.describe('helpers', () => {
+    test.describe('toJSON', () => {
+      test.it('should return the received object converted to JSON stringified string', () => {
+        const fooTemplates = templates.compile({template: '{{toJSON fooVar}}'})
+        const templateResult = fooTemplates.template({
+          fooVar: {
+            fooKey: 'fooValue'
+          }
+        })
+
+        test.expect(templateResult.replace(/\s/gmi, '')).to.equal('{"fooKey":"fooValue"}')
+      })
+    })
+
+    test.describe('capitalize', () => {
+      test.it('should return the string capitalized', () => {
+        const fooTemplates = templates.compile({template: '{{capitalize fooVar}}'})
+        const templateResult = fooTemplates.template({
+          fooVar: 'foo Phrase'
+        })
+
+        test.expect(templateResult).to.equal('Foo phrase')
+      })
+    })
+
+    test.describe('upperCase', () => {
+      test.it('should return the string converted to upperCase', () => {
+        const fooTemplates = templates.compile({template: '{{upperCase fooVar}}'})
+        const templateResult = fooTemplates.template({
+          fooVar: 'foo Phrase'
+        })
+
+        test.expect(templateResult).to.equal('FOO PHRASE')
+      })
+    })
+
+    test.describe('startCase', () => {
+      test.it('should return the string converted to startCase', () => {
+        const fooTemplates = templates.compile({template: '{{startCase fooVar}}'})
+
+        test.expect(fooTemplates.template({
+          fooVar: '--foo-bar--'
+        })).to.equal('Foo Bar')
+
+        test.expect(fooTemplates.template({
+          fooVar: 'fooBar'
+        })).to.equal('Foo Bar')
+
+        test.expect(fooTemplates.template({
+          fooVar: '__FOO_BAR__'
+        })).to.equal('FOO BAR')
+      })
+    })
+
+    test.describe('comma-separated', () => {
+      test.it('provided an array, it should return it converted to a string separated by commas', () => {
+        const fooTemplates = templates.compile({template: '{{comma-separated fooArray}}'})
+        test.expect(fooTemplates.template({
+          fooArray: ['a', 'b', 'c', 'd', 'e']
+        })).to.equal('a,b,c,d,e')
       })
     })
   })
