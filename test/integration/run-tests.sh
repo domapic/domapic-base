dd3bc26ae9ba76700d9b1a0ae748acf3c120b427
Javier Brea <javier.brea@gmail.com>
2018-03-02 20:40:02 +0100

    Integration tests. Remove -i attr before delete it

--- test/integration/run-tests.sh
+++ test/integration/run-tests.sh
@@ -8,7 +8,7 @@ export test_to_run="basics"
 docker-compose up --build --exit-code-from test
 
 sudo chattr -i .config_volume/.domapic
-rm -r .config_volume/.domapic
+rm -rf .config_volume/.domapic
 service_to_start="start"
 command_to_use="node"
 options_to_use="log-level"
