db25ddf4d20ab5ee53cbb20e71546d30a21b7806
Javier Brea <javier.brea@gmail.com>
2018-03-02 11:50:31 +0100

    Add docker integration tests

---
+++ test/integration/.config_volume/readme.md
@@ -0,0 +1,3 @@
+
+This is a shared folder between services and integration tests.
+This folder is used by services as home path, so they create configuration here, and can be checked in integration tests.
---
+++ test/integration/service/start/server.js
@@ -0,0 +1,9 @@
+
+const path = require('path')
+const domapic = require('./index.js')
+
+new domapic.Service({
+  packagePath: path.resolve(__dirname)
+}).then((service) => {
+  return service.server.start()
+})
