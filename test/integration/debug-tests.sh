079ce80d1868c47b4a1898b91047cc0fba418b00
Javier Brea <javier.brea@gmail.com>
2018-03-02 14:47:26 +0100

    Use one script for launch all tests, other for local tests development

---
+++ test/integration/debug-tests.sh
@@ -0,0 +1,29 @@
+ #!/usr/bin/env bash
+
+export test_to_run
+export service_to_start
+export options_to_use
+export command_to_use
+
+test_to_launch=$1
+debug_mode=$2
+exit_instruction="--exit-code-from test"
+
+if [[ -n "$test_to_launch" && "$debug_mode" = "alive" ]]; then
+  exit_instruction=""
+fi
+
+function launch_test {
+  local test_name=$1
+  service_to_start=$2
+  command_to_use=$3
+  options_to_use=$4
+  test_to_run=$5
+
+  if [[ ! -n "$test_to_launch" || (-n "$test_to_launch" && "$test_to_launch" = "$test_name") ]]; then
+    echo "Launching integration test \"${test_name}\""
+    docker-compose up --build ${exit_instruction}
+  fi
+}
+
+launch_test "start" "start" "node" "start" "start"
