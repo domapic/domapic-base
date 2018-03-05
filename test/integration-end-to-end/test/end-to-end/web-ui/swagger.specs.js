0025c74b433c3b265f1f971163b2e71893ca1cb3
Javier Brea <javier.brea@gmail.com>
2018-03-05 14:48:27 +0100

    Add puppeteer end-to-end tests structure

--- test/integration-end-to-end/test/end-to-end/web-ui/swagger.specs.js
+++ test/integration-end-to-end/test/end-to-end/web-ui/swagger.specs.js
@@ -1,6 +1,36 @@
 
+const puppeteer = require('puppeteer')
 const test = require('./test/unit/index')
 
 test.describe('Swagger Web UI', function () {
+  let page
+  let browser
 
+  test.before(() => {
+    return puppeteer.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    })
+    .then((br) => {
+      browser = br
+      return browser.newPage()
+        .then((pg) => {
+          page = pg
+          return page.goto('http://service:3000')
+        })
+    })
+  })
+
+  test.after(() => {
+    return browser.close()
+  })
+
+  test.it('should load the swagger interface', () => {
+    return page.content()
+      .then((content) => {
+        console.log(content)
+      })
+  })
 })
