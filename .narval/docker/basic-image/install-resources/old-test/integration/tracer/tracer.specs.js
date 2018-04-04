36092c666e1ea5280b13edc57968e5bca73d8279
Javier Brea <javier.brea@gmail.com>
2018-04-04 20:00:16 +0200

    Testing narval.yml

---
+++ .narval/docker/basic-image/install-resources/old-test/integration/tracer/tracer.specs.js
@@ -0,0 +1,38 @@
+
+const fs = require('fs')
+const path = require('path')
+const Promise = require('bluebird')
+
+const test = require('mocha-sinon-chai')
+const config = require('../../utils/config')
+
+const getTodayDate = function () {
+  return new Date().toISOString().slice(0, 10).replace(/-/g, '')
+}
+
+const getTracesFilePath = function () {
+  return path.resolve(config.paths.domapicConfig, 'logs', 'service.' + getTodayDate() + '.log')
+}
+
+test.describe('Tracer', function () {
+  const traceFilePath = getTracesFilePath()
+
+  test.describe('Daily file', function () {
+    test.it('should save traces into a file containing the date', () => {
+      return test.expect(fs.existsSync(traceFilePath)).to.be.true()
+    })
+  })
+
+  test.describe('Traces', function () {
+    let tracesFileContent = fs.readFileSync(traceFilePath, {
+      encoding: 'utf8'
+    })
+
+    test.it('should log only traces with trace level', () => {
+      return Promise.all([
+        test.expect(tracesFileContent).not.to.include('[info]'),
+        test.expect(tracesFileContent).to.include('[warn]')
+      ])
+    })
+  })
+})
