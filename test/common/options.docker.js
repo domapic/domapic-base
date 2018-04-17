e6087133fa916be63bd93f23ab889ecc097fccd3
Javier Brea <javier.brea@gmail.com>
2018-04-17 19:54:32 +0200

    Tests adapted to new Narval version

--- test/common/options.docker.js
+++ test/common/options.docker.js
@@ -4,14 +4,14 @@ const path = require('path')
 module.exports = {
   service: {
     protocol: 'http://',
-    host: 'service',
+    host: 'service-container',
     port: '3000'
   },
   paths: {
     domapicConfig: path.resolve(__dirname, '..', '..', '.shared', '.domapic', 'service')
   },
   explicitServiceOptions: {
-    path: '/app/.shared',
-    hostName: 'service'
+    path: '/narval/.shared',
+    hostName: 'service-container'
   }
 }
