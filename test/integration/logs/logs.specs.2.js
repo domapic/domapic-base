01fadfa796fad7966490e039000f7579fc1dd0a0
Javier Brea <javier.brea@gmail.com>
2018-04-27 13:28:29 +0200

    Testing multiple specs for one suite in Narval

--- test/integration/logs/logs.specs.2.js
+++ test/integration/logs/logs.specs.2.js
@@ -7,7 +7,7 @@ const test = require('narval')
 test.describe('Service logs', function () {
   const logsPath = path.resolve(__dirname, '..', '..', '..', '.narval', 'logs', 'integration', 'logs', 'timeout')
 
-  test.it('exit-code.log file should exist', () => {
-    return test.expect(fs.existsSync(path.join(logsPath, 'exit-code.log'))).to.be.true()
+  test.it('output.log file should exist', () => {
+    return test.expect(fs.existsSync(path.join(logsPath, 'output.log'))).to.be.true()
   })
 })
