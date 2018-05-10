e1591d481fe4c6bbec0132895076f8a0f3cab3df
Javier Brea <javier.brea@gmail.com>
2018-05-10 17:59:06 +0200

    Fix integration tests

--- test/commands/clean.sh
+++ test/commands/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm i
+rm -rf .test
--- test/commands/start.sh
+++ test/commands/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-node test/app/start.js --name=${host_name} --path=${app_path} ${extra_options}
+node test/app/start.js --name=service --host=${host_name} --path=${app_path} ${extra_options}
